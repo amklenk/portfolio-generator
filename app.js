c6e48039be206f1cb538f76784d063f11e749209
Amanda Klenk <amandamklenk3@gmail.com>
2022-06-06 06:26:08 -0400

    created functions to render featured and non-featured projects

--- app.js
+++ app.js
@@ -139,18 +139,16 @@ const promptProject = portfolioData => {
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+fs.writeFile('./index.html', pageHTML, err => {
+    if (err) throw new Error(err);
+
+    console.log('Page created! Check out index.html to see it!');
+  });
   });
 
 
-//To be uncommented later
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//     if (err) throw new Error (err);
-
-//     console.log('Portfolio complete! Check out index.html to see the output!');
-//   });
 
 
 
